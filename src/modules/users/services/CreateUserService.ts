939d5e3f9e682ef4c955771f8f0c84c3eae63367
JoaoPaiva <jvlimapaiva@gmail.com>
2022-08-15 19:58:45 -0300

    Adicionando criptografia hash nas senhas.

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from "@shared/errors/AppError";
+import { hash } from "bcryptjs";
 import { getCustomRepository } from "typeorm"
 import User from "../typeorm/entities/User";
 import UsersRepository from "../typeorm/repositories/User.Repository";
@@ -20,12 +21,16 @@ class CreateUserService {
     const emailExists = await usersRepository.findByEmail(email);
 
     if(emailExists){
-      throw new AppError('There is already one user with this email registered!');
+      throw new AppError('User already exists!');
     }
+
+    //criando o hash da senha antes de inserir no banco.
+    const hashedPassword = await hash(password, 8)
+
     const user = await usersRepository.create({
       name,
       email,
-      password
+      password: hashedPassword
     });
 
     await usersRepository.save(user)
