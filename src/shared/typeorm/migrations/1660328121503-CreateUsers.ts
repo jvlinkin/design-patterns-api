e2f0335cc2f7cf824d81ba0d877da1938543f6e8
JoaoPaiva <jvlimapaiva@gmail.com>
2022-08-15 17:02:56 -0300

    Criação de migration, entities, e repositories de Users

--- src/shared/typeorm/migrations/1660328121503-CreateUsers.ts
+++ src/shared/typeorm/migrations/1660328121503-CreateUsers.ts
@@ -1,11 +1,52 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateUsers1660328121503 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
+          },
+          {
+            name: 'name',
+            type: 'varchar',            
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'avatar',
+            type:'varchar',
+            isNullable: true
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp with time zone',
+            default: 'now()'
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp with time zone',
+            default: 'now()'
+          },
+        ]
+      }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('users');
     }
 
 }
