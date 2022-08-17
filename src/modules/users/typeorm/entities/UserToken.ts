321cd9a4b0cb721e75eca8a7962b247f585ceaa4
JoaoPaiva <jvlimapaiva@gmail.com>
2022-08-16 21:21:07 -0300

    migrations, entities, e repositories  - user_tokens

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,24 @@
+
+import { Column, CreateDateColumn, Entity, Generated, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('users')
+class UserToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+}
+
+export default UserToken;
