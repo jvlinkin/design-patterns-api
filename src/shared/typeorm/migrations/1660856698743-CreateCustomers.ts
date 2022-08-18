85510635e9c0d6994e311b48bd8d4821162296c0
JoaoPaiva <jvlimapaiva@gmail.com>
2022-08-18 18:28:54 -0300

    Customers - Migration, Rep, Entities

---
+++ src/shared/typeorm/migrations/1660856698743-CreateCustomers.ts
@@ -0,0 +1,46 @@
+
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateCustomers1660856698743 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+        name: 'customers',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp with time zone',
+            default: 'now()'
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp with time zone',
+            default: 'now()'
+          },
+        ]
+      }))
+      
+    };
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('customers');
+    }
+
+}
