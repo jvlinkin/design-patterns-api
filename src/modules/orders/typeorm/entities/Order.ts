13b7b1d2a441c032392ce2f6822a129f907cc0d0
JoaoPaiva <jvlimapaiva@gmail.com>
2022-08-22 17:48:42 -0300

    Entidades de Order e OrderProducts

--- src/modules/orders/typeorm/entities/Order.ts
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -1,6 +1,15 @@
+import {
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
-import Customer from "@modules/customers/typeorm/entities/Customer";
-import { CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import Customer from '@modules/customers/typeorm/entities/Customer';
+import OrdersProducts from './OrdersProducts';
 
 @Entity('orders')
 class Order {
@@ -8,15 +17,18 @@ class Order {
   id: string;
 
   @ManyToOne(() => Customer)
-  @JoinColumn({name: 'customer_id'})
-  customer: Customer
+  @JoinColumn({ name: 'customer_id' })
+  customer: Customer;
+
+  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
+    cascade: true,
+  })
+  order_products: OrdersProducts[];
 
   @CreateDateColumn()
   createdAt: Date;
 
   @UpdateDateColumn()
   updatedAt: Date;
-
 }
-
 export default Order;
