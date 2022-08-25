207f0f9f7ef70faeb06a068e78a29f64d9c52b01
JoaoPaiva <jvlimapaiva@gmail.com>
2022-08-25 16:17:01 -0300

    Implementação do Redis - Save, Recover

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,6 +1,7 @@
 import { getCustomRepository } from "typeorm"
 import Product from "../typeorm/entities/Product";
 import { ProductRepository } from "../typeorm/repositories/ProductsRepository"
+import RedisCache from "@shared/cache/RedisCache";
 
 
 class ListProductService {
@@ -8,8 +9,18 @@ class ListProductService {
     
     //Importando o repositório.
     const productsRepository = getCustomRepository(ProductRepository);
-    
-    const products = await productsRepository.find();
+    //Instanciando o Redis:
+    const redisCache = new RedisCache();
+
+    //Tentando recuperar os dados no Cache, passando a chave como parêmetro:
+    let products = await redisCache.recover<Product[]>('api-vendas-PRODUCT-LIST');
+
+    if(!products){//Se não for encontrado nada, então ele busca no Postgres, 
+      products = await productsRepository.find();
+      //e salva no cache do Redis.
+      await redisCache.save('api-vendas-PRODUCT-LIST',products)
+    }
+  
     return products;
 
   }
--- src/shared/cache/RedisCache.ts
+++ src/shared/cache/RedisCache.ts
@@ -1,5 +1,5 @@
 import Redis, {Redis as RedisClient} from 'ioredis';
-import cacheConfig from '@config/cache';
+import cacheConfig from '@config/cache'; 
 
 export default class RedisCache{
   private client: RedisClient;
@@ -9,10 +9,26 @@ export default class RedisCache{
   }
 
   public async save(key:string, value: any): Promise<void>{
-    console.log(key,value);
+    //Método SET é do Redis, para realizar uma ação no BD.
+    await this.client.set(key, JSON.stringify(value));
   }
 
-  //public async recover<T>(key:string): Promise<T | null>{}
+  //Como no momento que fazemos o recover (lá no serviço que usa o cache do Redis), e não sabemos os dados que serão retornados, criamos aqui o método recover com o parâmetro<T>, que seria um tipo genérico.
+  public async recover<T>(key:string): Promise<T | null>{
+    //Método GET é do Redis, para buscar no BD.
+    const data = await this.client.get(key);
+    
+    if(!data){
+      return null
+    }
 
-  public async invalidate(key:string): Promise<void>{}
+    //Nesse momento, quando os dados já estão no Redis, salvos como string (configuração do método save), precisamos converter para o tipo que foi passado no momento de executar o método recover:
+    const parsedData = JSON.parse(data) as T;
+    return parsedData;
+
+  }
+
+  public async invalidate(key:string): Promise<void>{
+    await this.client.get(key);
+  }
 }
